1bf8f69922348606d6c1ee5a195edcd248dd2039
Niceplugin <niceplugin@gmail.com>
2024-01-29 23:45:51 +0900

    Update ko (#77)
    * docs: remove VM banner
    
    * docs: delete useless import (#2120)

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,5 +1,5 @@
 import { h } from 'vue'
-import { Theme, useData } from 'vitepress'
+import { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import AsideSponsors from './components/AsideSponsors.vue'
 // import HomeSponsors from './components/HomeSponsors.vue'
@@ -7,7 +7,6 @@ import './styles/vars.css'
 import './styles/sponsors.css'
 import VueSchoolLink from './components/VueSchoolLink.vue'
 import VueMasteryLogoLink from './components/VueMasteryLogoLink.vue'
-import VueMasteryBanner from './components/VueMasteryBanner.vue'
 
 const theme: Theme = {
   ...DefaultTheme,
@@ -15,7 +14,6 @@ const theme: Theme = {
     return h(DefaultTheme.Layout, null, {
       // 'home-features-after': () => h(HomeSponsors),
       'aside-ads-before': () => h(AsideSponsors),
-      'layout-top': () => h(VueMasteryBanner),
     })
   },
 
